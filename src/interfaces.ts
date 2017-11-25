e9289d52c9a3a5ba9b7fb6d1153c52cefb88c0bb
Homa Wong <homawong@gmail.com>
2017-11-25 02:45:59 -0800

    fix: improve Expecter type to better support inference. (#3)

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,10 @@
 export type Predicate = (value: any) => boolean
-export type Expecter = ExpecterHash | ExpecterNode[]
 
-export type ExpecterNode = boolean | number | string | RegExp | Predicate | ExpecterHash
-
-export type ExpecterHash = { [i: string]: ExpecterNode | ExpecterNode[] }
+export type Expecter<T extends Struct = Struct> = ExpecterHash<T> | ExpecterHash<T>[]
+export type ExpecterNode<T extends Struct = Struct> = undefined | boolean | number | string | RegExp | Predicate | ExpecterHash<T>
+export type ExpecterHash<T extends Struct = Struct> = {
+  [P in keyof T]: ExpecterNode<T[P]> | ExpecterNode<T[P]>[];
+}
 
 export type Struct = StructNode | StructHash | (StructNode | StructHash)[]
 
