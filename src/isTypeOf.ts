9f2c935e90ae60f2aa369ad15a112c73ea492711
unional <homawong@gmail.com>
2017-11-26 11:37:46 -0800

    chore: misc update on tslint disable string

--- src/isTypeOf.ts
+++ src/isTypeOf.ts
@@ -1,6 +1,6 @@
 export function isTypeOf(x: 'number' | 'boolean' | 'string') {
   return Object.assign(
-    // tslint:disable-next-line
+    // tslint:disable-next-line:strict-type-predicates
     (a) => typeof a === x,
     {
       toString() {
