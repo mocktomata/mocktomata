4f96167026d9c6ec7988aecce3d3193f28b2f2da
Homa Wong <homawong@gmail.com>
2018-01-22 22:25:50 -0800

    build: move files under javascript
    so each language will be in their own folder.

--- scripts/run-on-latest-node.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-latest_version=$(npm info node version);
-node_version=$(node -v);
-if [ ${node_version:1:1} = ${latest_version:0:1} ]; then
-  eval $1;
-else
-  echo "NodeJS ${node_version} instead of latest (${latest_version:0:1}) is detected. Skipping command";
-fi
