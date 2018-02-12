5d6b669f615f4331f9b052cdbbaeb1bc915a8577
Homa Wong <homawong@gmail.com>
2018-02-11 19:41:35 -0800

    fix: remove ws.
    Moved to komondor-plugin-ws

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,14 @@ export * from './io'
 export * from './spec'
 
 import { registerPlugin } from './plugin'
+export { registerPlugin }
 import * as childProcess from './childProcess'
 import * as genericClass from './class'
 import * as genericFunction from './function'
 import * as promise from './promise'
 import * as stream from './stream'
-import * as ws from './ws'
+// import * as ws from './ws'
+import { KOMONDOR_CONFIG } from './constants'
 
 // order is important, top is generic, bottom is specific.
 registerPlugin(promise)
@@ -19,4 +21,11 @@ registerPlugin(stream)
 registerPlugin(childProcess)
 registerPlugin(genericFunction)
 registerPlugin(genericClass)
-registerPlugin(ws)
+// registerPlugin(ws)
+
+if (KOMONDOR_CONFIG && KOMONDOR_CONFIG.plugins) {
+  KOMONDOR_CONFIG.plugins.forEach(p => {
+    const m = require(p)
+    registerPlugin(m)
+  })
+}
