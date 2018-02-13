d23e7bd4357ee51f99dbd8afa13452f21df00768
Homa Wong <homawong@gmail.com>
2018-02-12 18:24:39 -0800

    fix: SimulateMatch takes expectedAction as SpecAction

--- src/errors.spec.ts
+++ src/errors.spec.ts
@@ -6,7 +6,7 @@ test('SimulationMismatch error', t => {
   const action = { type: 'fn/callback', payload: { a: 1 }, meta: { functionId: 3 } }
   const err = new SimulationMismatch('some id', 'fn/invoke', action)
   t.is(err.id, 'some id')
-  t.is(err.expectedActionType, 'fn/invoke')
+  t.is(err.expectedAction, 'fn/invoke')
   t.is(err.receivedAction, action)
   t.is(err.message, `Recorded data for 'some id' doesn't match with simulation. Expecting action type 'fn/invoke' but received: { type: 'fn/callback', payload: { a: 1 }, meta: { functionId: 3 } }`)
 })
@@ -14,6 +14,16 @@ test('SimulationMismatch error', t => {
 test('SimulationMismatch error, received action optional', t => {
   const err = new SimulationMismatch('some id', 'fn/invoke')
   t.is(err.id, 'some id')
-  t.is(err.expectedActionType, 'fn/invoke')
+  t.is(err.expectedAction, 'fn/invoke')
   t.is(err.message, 'Recorded data for \'some id\' doesn\'t match with simulation. Expecting action type \'fn/invoke\' but received: undefined')
+})
+
+test('SimulationMismatch error with expected action as SpecAction', t => {
+  const expectedAction = { type: 'fn/invoke', payload: [0, 'a'] }
+  const action = { type: 'fn/callback', payload: { a: 1 }, meta: { functionId: 3 } }
+  const err = new SimulationMismatch('some id', expectedAction, action)
+  t.is(err.id, 'some id')
+  t.is(err.expectedAction, expectedAction)
+  t.is(err.receivedAction, action)
+  t.is(err.message, `Recorded data for 'some id' doesn't match with simulation. Expecting action type { type: 'fn/invoke', payload: [0, 'a'] } but received: { type: 'fn/callback', payload: { a: 1 }, meta: { functionId: 3 } }`)
 })
--- src/errors.ts
+++ src/errors.ts
@@ -39,8 +39,8 @@ export class GivenSaveRequireSpecId extends Error {
 
 export class SimulationMismatch extends Error {
   // istanbul ignore next
-  constructor(public id: string, public expectedActionType: string, public receivedAction?: SpecAction) {
-    super(`Recorded data for '${id}' doesn't match with simulation. Expecting action type '${expectedActionType}' but received: ${tersify(receivedAction)}`)
+  constructor(public id: string, public expectedAction: string | SpecAction, public receivedAction?: SpecAction) {
+    super(`Recorded data for '${id}' doesn't match with simulation. Expecting action type ${tersify(expectedAction)} but received: ${tersify(receivedAction)}`)
 
     Object.setPrototypeOf(this, new.target.prototype)
   }
