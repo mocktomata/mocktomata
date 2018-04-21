fac52e9ce9e18657ad5d7af126df4463a95a5fa2
Homa Wong <homawong@gmail.com>
2018-04-20 19:41:57 -0700

    Remove unused reference

--- src/spec.spec.ts
+++ src/spec.spec.ts
@@ -1,5 +1,5 @@
 import t from 'assert'
-import a, { AssertOrder } from 'assertron'
+import a from 'assertron'
 import { SimulationMismatch } from 'komondor-plugin'
 
 import { spec, SpecNotFound, NotSpecable } from '.'
