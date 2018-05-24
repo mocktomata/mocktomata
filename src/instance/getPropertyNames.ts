77fadb8a7990d92199363bd16d50abed70283735
Homa Wong <homawong@gmail.com>
2018-05-23 17:34:36 -0700

    fix: error instance support at return spot (#168)

--- src/instance/getPropertyNames.ts
+++ src/instance/getPropertyNames.ts
@@ -5,3 +5,15 @@ export function getPropertyNames(subject) {
   if (proto === null) return []
   return properties.concat(getPropertyNames(proto))
 }
+
+export function getProperties(subject) {
+  const propertyNames = getPropertyNames(subject)
+
+  return propertyNames.reduce((p, name) => {
+    p[name] = {
+      descriptor: Object.getOwnPropertyDescriptor(subject, name),
+      type: typeof subject[name]
+    }
+    return p
+  }, {})
+}
