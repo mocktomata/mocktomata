6e3e3dd421926410ceec6d6a1715bca1f2507023
Homa Wong <homawong@gmail.com>
2018-07-13 17:16:20 -0700

    fix: case when the subject has no constructor

--- src/instance/isInstance.spec.ts
+++ src/instance/isInstance.spec.ts
@@ -14,6 +14,7 @@ test('object literal is false', () => {
   t.equal(isInstance({}), false)
   t.equal(isInstance({ a: 1 }), false)
 })
+
 test('Empty class instance is true', () => {
   class Foo { }
   t(isInstance(new Foo()))
--- src/instance/isInstance.ts
+++ src/instance/isInstance.ts
@@ -2,5 +2,8 @@ export function isInstance(subject) {
   if (subject === undefined || subject === null) return false
   if (typeof subject !== 'object') return false
 
-  return subject.constructor.name !== 'Object'
+  // Encounter a case when using `axios` that it creates an object with no constructor.
+  // Don't know how it does that but it happens
+  // istanbul ignore next
+  return subject.constructor && subject.constructor.name !== 'Object'
 }
