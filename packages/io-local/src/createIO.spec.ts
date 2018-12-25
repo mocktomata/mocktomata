a368d8d900d62c45532305626a919e2a078b3728
Homa Wong (unional) <homawong@gmail.com>
2018-12-25 05:06:10 -0800

    update io-local for io-fs changes.
    Clean up to use standard `_deps` to hold all dependencies.

--- packages/io-local/src/createIO.spec.ts
+++ packages/io-local/src/createIO.spec.ts
@@ -5,7 +5,7 @@ describe('readSpec()', () => {
   test('returns an object', async () => {
     const io = createIO()
     const expected = { actions: [], expectation: 'abc' };
-    io.io.readSpec = (() => Promise.resolve(JSON.stringify(expected))) as any
+    io._deps.spec.read = (() => Promise.resolve(JSON.stringify(expected))) as any
 
     const actual = await io.readSpec('abc')
     t.deepStrictEqual(actual, expected)
--- packages/io-local/src/createIO.ts
+++ packages/io-local/src/createIO.ts
@@ -1,27 +1,28 @@
-import { loadConfig, readScenario, readSpec, writeScenario, writeSpec } from '@komondor-lab/io-fs';
+import { loadConfig, createSpecIO, createScenarioIO } from '@komondor-lab/io-fs';
 import { SpecRecord } from './interfaces';
 
-export function createIO() {
+export function createIO({ cwd } = { cwd: process.cwd() }) {
+  const spec = createSpecIO({ cwd })
+  const scenario = createScenarioIO({ cwd })
+
   return {
     async readSpec(id: string): Promise<SpecRecord> {
-      const specStr = await this.io.readSpec(id)
+      const specStr = await this._deps.spec.read(id)
       return JSON.parse(specStr)
     },
     async writeSpec(id: string, record: SpecRecord) {
-      return this.io.writeSpec(id, JSON.stringify(record))
+      return this._deps.spec.write(id, JSON.stringify(record))
     },
     async readScenario(id: string): Promise<any> {
-      const specStr = await this.io.readScenario(id)
+      const specStr = await this._deps.scenario.read(id)
       return JSON.parse(specStr)
     },
     async writeScenario(id: string, record: any) {
-      return this.io.writeScenario(id, JSON.stringify(record))
+      return this._deps.scenario.write(id, JSON.stringify(record))
     },
     async loadConfig() {
-      return Promise.resolve(this.io.loadConfig(process.cwd()))
+      return Promise.resolve(this._deps.loadConfig(process.cwd()))
     },
-    // do this instead of `import * as io` so that each object has its own copy,
-    // thus tests would not override each other.
-    io: { loadConfig, readScenario, readSpec, writeScenario, writeSpec }
+    _deps: { loadConfig, spec, scenario }
   }
 }
