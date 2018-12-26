043b7fc2e5180f3392fbebbc1debdeda761e79cf
Homa Wong (unional) <homawong@gmail.com>
2018-12-26 02:14:42 -0800

    test: minor fix. the plugin package is not in use and likely to be removed

--- packages/plugin/src/SpecExpectation.spec.ts
+++ packages/plugin/src/SpecExpectation.spec.ts
@@ -6,17 +6,17 @@ describe('createExpectation', () => {
   test('create with meta', () => {
     const s = createExpectation('node-fetch', 'invoke', { b: 1 })
     const expectation = s('payload', { a: 1 })
-    a.satisfy({ type: 'node-fetch', name: 'invoke', payload: 'payload', meta: { a: 1, b: 1 } }, expectation)
+    a.satisfies({ type: 'node-fetch', name: 'invoke', payload: 'payload', meta: { a: 1, b: 1 } }, expectation)
   })
   test('can use expectation with meta', () => {
     const s = createExpectation('node-fetch', 'invoke')
     const expectation = s('payload', { a: 1 })
-    a.satisfy({ type: 'node-fetch', name: 'invoke', payload: 'payload', meta: { a: 1 } }, expectation)
+    a.satisfies({ type: 'node-fetch', name: 'invoke', payload: 'payload', meta: { a: 1 } }, expectation)
   })
   test('can use expectation without meta', () => {
     const s = createExpectation('node-fetch', 'invoke')
     const expectation = s('payload')
-    a.satisfy({ type: 'node-fetch', name: 'invoke', payload: 'payload' }, expectation)
+    a.satisfies({ type: 'node-fetch', name: 'invoke', payload: 'payload' }, expectation)
   })
 })
 
@@ -25,19 +25,19 @@ describe('createScopedCreateExpectation', () => {
     const createSatisfier = createScopedCreateExpectation('x')
     const s = createSatisfier('node-fetch', 'invoke', { b: 1 })
     const expectation = s('payload', { a: 1 })
-    a.satisfy({ type: 'x/node-fetch', name: 'invoke', payload: 'payload', meta: { a: 1, b: 1 } }, expectation)
+    a.satisfies({ type: 'x/node-fetch', name: 'invoke', payload: 'payload', meta: { a: 1, b: 1 } }, expectation)
   })
   test('create with meta', () => {
     const createSatisfier = createScopedCreateExpectation('x')
     const s = createSatisfier('node-fetch', 'invoke')
     const expectation = s('payload', { a: 1 })
-    a.satisfy({ type: 'x/node-fetch', name: 'invoke', payload: 'payload', meta: { a: 1 } }, expectation)
+    a.satisfies({ type: 'x/node-fetch', name: 'invoke', payload: 'payload', meta: { a: 1 } }, expectation)
   })
   test('meta is optional', () => {
     const createSatisfier = createScopedCreateExpectation('x')
     const s = createSatisfier('node-fetch', 'invoke')
     const expectation = s('payload')
-    a.satisfy({ type: 'x/node-fetch', name: 'invoke', payload: 'payload' }, expectation)
+    a.satisfies({ type: 'x/node-fetch', name: 'invoke', payload: 'payload' }, expectation)
   })
 })
 
