efcf1b1dbbd35ff5696bf6ee87ae4a7ef1c915e1
Homa Wong (unional) <homawong@gmail.com>
2019-03-30 21:20:49 -0700

    feat(plugin): move PluginNotFound to plugin project

--- packages/plugin/src/errors.ts
+++ packages/plugin/src/errors.ts
@@ -1,5 +1,14 @@
 import { BaseError } from 'make-error';
 
+export class PluginNotFound extends BaseError {
+  // istanbul ignore next
+  constructor(public pluginName: string) {
+    super(`Could not locate plugin '${pluginName}'`)
+
+    Object.setPrototypeOf(this, new.target.prototype)
+  }
+}
+
 export class PluginAlreadyLoaded extends BaseError {
   // istanbul ignore next
   constructor(public pluginName: string) {
