ba32271bd45d6ec29720526327703587ea8ef3c6
Homa Wong (unional) <homawong@gmail.com>
2019-03-30 21:20:49 -0700

    feat(plugin): move PluginNotFound to plugin project

--- packages/plugin/src/loadPlugins.spec.ts
+++ packages/plugin/src/loadPlugins.spec.ts
@@ -1,6 +1,6 @@
 import { createLocalIO } from '@komondor-lab/io-local';
 import a from 'assertron';
-import { getPlugins, loadPlugins } from '.';
+import { getPlugins, loadPlugins, PluginNotFound } from '.';
 
 /**
  * Plugin order is reversed so that most specific plugin are checked first.
@@ -11,4 +11,9 @@ test('load plugins in reverse order', async () => {
   await loadPlugins({ io }, pluginNames)
   const actual = getPlugins()
   a.satisfies(actual.map(p => p.name), ['@komondor-lab/plugin-fixture-deep-link/pluginA', '@komondor-lab/plugin-fixture-dummy'])
+})
+
+test('Not existing plugin throws PluginNotFound', async () => {
+  const io = createLocalIO()
+  await a.throws(() => loadPlugins({ io }, ['not-exist']), PluginNotFound)
 })
--- packages/plugin/src/loadPlugins.ts
+++ packages/plugin/src/loadPlugins.ts
@@ -1,5 +1,6 @@
 import { registerPlugin } from './registerPlugin';
 import { PluginModule } from './interfaces';
+import { PluginNotFound } from './errors';
 
 export type PluginContext = {
   io: {
@@ -12,7 +13,16 @@ export type PluginContext = {
  */
 export async function loadPlugins({ io }: PluginContext, pluginNames: string[]) {
   return Promise.all(pluginNames.map(async name => {
-    const m = await io.loadPlugin(name)
+    const m = await tryLoad({ io }, name)
     registerPlugin(name, m)
   }))
+}
+
+async function tryLoad({ io }: PluginContext, name: string) {
+  try {
+    return await io.loadPlugin(name)
+  }
+  catch {
+    throw new PluginNotFound(name)
+  }
 }
