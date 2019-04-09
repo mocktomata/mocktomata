ba6e9cef0059ee0f5d32f4431c8c849ab48c39fb
Homa Wong (unional) <homawong@gmail.com>
2019-04-08 21:56:29 -0700

    feat(io-fs): expose Repository type

--- packages/io-fs/src/createFileRepository.ts
+++ packages/io-fs/src/createFileRepository.ts
@@ -1,10 +1,20 @@
+import { PluginModule } from '@komondor-lab/plugin';
 import path from 'path';
 import { getConfig } from './config';
+import { createPluginRepository } from './plugin';
 import { createScenarioRepository } from './scenario';
 import { createSpecRepository } from './spec';
-import { createPluginRepository } from './plugin';
 
-export function createFileRepository(cwd: string) {
+export type Repository = {
+  readSpec(id: string): Promise<string>
+  writeSpec(id: string, data: string): Promise<void>
+  readScenario(id: string): Promise<string>
+  writeScenario(id: string, data: string): Promise<void>
+  getPluginList(): Promise<string[]>
+  loadPlugin(name: string): Promise<PluginModule>
+}
+
+export function createFileRepository(cwd: string): Repository {
   const config = getConfig(cwd)
   const komondorFolder = path.resolve(cwd, config.komondorFolder)
   const spec = createSpecRepository(komondorFolder)
