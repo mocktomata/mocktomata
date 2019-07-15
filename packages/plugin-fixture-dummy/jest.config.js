9523327af4741cd2e78ccc58b77389d27cc5b740
Homa Wong (unional) <homawong@gmail.com>
2019-07-14 18:04:59 -0700

    build(dummy): misc cleanup

--- packages/plugin-fixture-dummy/jest.config.js
+++ packages/plugin-fixture-dummy/jest.config.js
@@ -2,11 +2,8 @@ const base = require('@unional/devpkg-node/simple/config/jest.common')
 
 module.exports = {
   ...base,
-  name: 'plugin-fixture-dummy',
   displayName: 'plugin-fixture-dummy',
   roots: [
     '<rootDir>/src'
-  ],
-  testEnvironment: 'node',
-  testMatch: ['**/*.spec.ts', '**/*.node-spec.ts'],
+  ]
 }
