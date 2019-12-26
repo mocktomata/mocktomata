5553e5645b46544d044ca13c790e5f32ada2b30d
Homa Wong <homawong@gmail.com>
2019-12-25 19:40:57 -0800

    test(file-server): fix test

--- packages/file-server/src/start.spec.ts
+++ packages/file-server/src/start.spec.ts
@@ -42,7 +42,7 @@ describe('server behavior', () => {
     const response = await fetch(buildUrl('info'))
     const actual = await response.text()
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const pjson = require('../package.json')
+    const pjson = require('../../mocktomata/package.json')
     t.strictEqual(actual, `{"name":"mocktomata","version":"${pjson.version}","url":"http://localhost:${server.info.port}","plugins":[]}`)
   })
 
