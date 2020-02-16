ec6d9d3e09cd798e49f300cb412834a7a71e09bc
Homa Wong <homawong@gmail.com>
2020-02-16 13:17:47 -0800

    chore: look for why test fails on window

--- packages/io-fs/src/util/readByHash.ts
+++ packages/io-fs/src/util/readByHash.ts
@@ -4,6 +4,7 @@ import path from 'path'
 export function readByHash(baseDir: string, id: string, hash: string, dupId = 0): string {
   const filename = dupId ? hash + dupId : hash
   const filePath = path.join(baseDir, filename)
+  console.info('readByHash', filePath)
   const content = fs.readFileSync(filePath, 'utf-8')
   const [firstLine, specStr] = content.split('\n', 2)
   if (firstLine !== id) {
