37c8a860ba7adf527ef3b837160409f819b41bc7
Homa Wong <homawong@gmail.com>
2020-02-16 15:14:15 -0800

    build: back to @jest-runner/electron
    and run nodejs tests only in CI
    
    electron test seems to have problem in ubuntu env./
    Likely related to display

--- .jest/jest.config.base.electron.js
+++ .jest/jest.config.base.electron.js
@@ -2,7 +2,7 @@ const base = require('./jest.config.base')
 
 module.exports = {
   ...base,
-  runner: 'jest-electron/runner',
-  testEnvironment: 'jest-electron/environment',
+  runner: '@jest-runner/electron/main',
+  testEnvironment: 'node',
   testMatch: ['**/?*.(spec|test|integrate|accept|system|unit)(.electron)?.[jt]s?(x)'],
 }
