74b4033d9b00df536f642abcf9e303fff7368546
Homa Wong <homawong@gmail.com>
2020-03-07 18:33:06 -0800

    fix: rename cli from mocktomata to mt
    keep it short and easy to use

--- packages/mocktomata/src/cli/cli.ts
+++ packages/mocktomata/src/cli/cli.ts
@@ -1,7 +1,7 @@
 import { createCli } from 'clibuilder'
-import { serveCommand } from './serveCommand';
+import { serveCommand } from './serveCommand'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pjson = require('../../package.json')
 
-export const cli = createCli({ name: 'mocktomata', version: pjson.version, commands: [serveCommand] })
+export const cli = createCli({ name: 'mt', version: pjson.version, commands: [serveCommand] })
