2bf408cf70ba2e9dc9105d9cec09d1f1058433e8
Homa Wong <homawong@gmail.com>
2020-03-22 18:07:03 -0700

    test: loosen time check

--- packages/framework/src/timeTracker/createTimeTracker.spec.ts
+++ packages/framework/src/timeTracker/createTimeTracker.spec.ts
@@ -40,7 +40,7 @@ test('duration returns total duration since first elaspe() call', async () => {
 
   timeTracker.stop()
   expect(actual).toBeGreaterThanOrEqual(10)
-  expect(actual).toBeLessThanOrEqual(200)
+  expect(actual).toBeLessThanOrEqual(2000)
 })
 
 test('elaspe() returns time passed since last elaspe() call', async () => {
