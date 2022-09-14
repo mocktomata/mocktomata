d55b103313b01f35131d0b1e7973e78baadb4e49
Homa Wong <homawong@gmail.com>
2022-09-14 02:40:02 -0700

    test: remvoe extra test
    The same test is in `simulator.spec.ts`

--- packages/framework/ts/spec/record.spec.ts
+++
@@ -1,7 +0,0 @@
-import { createSpecRecordValidator } from './record.js'
-import a from 'assertron'
-import { PluginsNotLoaded } from './errors.js'
-
-test('throw PluginsNotLoaded when loaded record uses not loaded plugins', () => {
-  a.throws(() => createSpecRecordValidator('', { refs: [{ plugin: 'unknown', profile: 'target' }], actions: [] }), PluginsNotLoaded)
-})
