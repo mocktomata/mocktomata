18c263ab9d91c0db16f2c95778aafdac31130e36
Homa Wong <homawong@gmail.com>
2022-10-15 00:54:58 -0700

    fix: add options to errors

--- packages/framework/ts/spec-plugin/errors.ts
+++ packages/framework/ts/spec-plugin/errors.ts
@@ -1,30 +1,31 @@
+import { ModuleError } from 'iso-error'
 import { MocktomataError } from '../errors.js'
 
 export class PluginNotFound extends MocktomataError {
   // istanbul ignore next
-  constructor(public pluginName: string) {
-    super(`Could not locate plugin '${pluginName}'`)
+  constructor(public pluginName: string, options?: ModuleError.Options) {
+    super(`Could not locate plugin '${pluginName}'`, options)
   }
 }
 
 export class DuplicatePlugin extends MocktomataError {
   // istanbul ignore next
-  constructor(public pluginName: string) {
-    super(`A plugin with the name '${pluginName}' has already been loaded.`)
+  constructor(public pluginName: string, options?: ModuleError.Options) {
+    super(`A plugin with the name '${pluginName}' has already been loaded.`, options)
   }
 }
 
 export class PluginModuleNotConforming extends MocktomataError {
   // istanbul ignore next
-  constructor(public pluginName: string) {
-    super(`${pluginName} is not a valid plugin module.`)
+  constructor(public pluginName: string, options?: ModuleError.Options) {
+    super(`${pluginName} is not a valid plugin module.`, options)
   }
 }
 
 export class PluginNotConforming extends MocktomataError {
   // istanbul ignore next
-  constructor(public pluginName: string) {
-    super(`${pluginName} is not a plugin.`)
+  constructor(public pluginName: string, options?: ModuleError.Options) {
+    super(`${pluginName} is not a plugin.`, options)
   }
 }
 
