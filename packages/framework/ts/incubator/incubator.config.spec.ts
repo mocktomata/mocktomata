abd744787caf0a6634265c0584e639c758788900
Homa Wong <homawong@gmail.com>
2022-10-14 23:13:27 -0700

    chore: rename mock files
    and exclude them from distribution

--- packages/framework/ts/incubator/incubator.config.spec.ts
+++ packages/framework/ts/incubator/incubator.config.spec.ts
@@ -1,5 +1,5 @@
-import { activate as decrementActivate } from './decrementPlugin.js'
-import { activate as incrementActivate } from './incrementPlugin.js'
+import { activate as decrementActivate } from './decrementPlugin.mock.js'
+import { activate as incrementActivate } from './incrementPlugin.mock.js'
 import { incubator } from './incubator.js'
 
 beforeAll(() => incubator.config({
