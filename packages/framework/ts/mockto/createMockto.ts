55162b561d42059c08c4a755b32bf97f0379cf4b
Homa Wong <homawong@gmail.com>
2022-10-16 01:09:22 -0700

    refactor: simplify code
    combine FixedMode and non-fixed mode creation

--- packages/framework/ts/mockto/createMockto.ts
+++ packages/framework/ts/mockto/createMockto.ts
@@ -1,10 +1,14 @@
 import type { AsyncContext } from 'async-fp'
+import { createMemoryLogReporter } from 'standard-log'
 import { transformConfig } from '../config/index.js'
+import { createLogContext } from '../log/createLogContext.js'
 import { loadPlugins } from '../spec-plugin/index.js'
 import type { Spec } from '../spec/index.js'
+import { createSpecObject, getEffectiveSpecModeContext } from '../spec/index.js'
+import { getCallerRelativePath } from '../test-utils/index.js'
 import type { TimeTracker } from '../timeTracker/index.js'
 import type { Mocktomata } from '../types.js'
-import { createFixedModeMocktoFn, createMocktoFn } from './createMocktoFn.js'
+import { resolveMocktoFnArgs } from './resolveMocktoFnArgs.js'
 
 export namespace createMockto {
   export type Mockto = MocktoFn & {
@@ -31,16 +35,36 @@ export function createMockto(context: AsyncContext<Mocktomata.Context>): createM
     .extend(loadPlugins)
     .extend(transformConfig)
     .extend({ timeTrackers: [] as TimeTracker[] })
+
   return Object.assign(
     createMocktoFn(ctx),
     {
-      live: createFixedModeMocktoFn(ctx, 'live'),
-      save: createFixedModeMocktoFn(ctx, 'save'),
-      simulate: createFixedModeMocktoFn(ctx, 'simulate'),
+      live: createMocktoFn(ctx, 'live'),
+      save: createMocktoFn(ctx, 'save'),
+      simulate: createMocktoFn(ctx, 'simulate'),
       async teardown() {
         const { timeTrackers } = await ctx.get()
         timeTrackers.forEach(t => t.terminate())
       },
     }
   )
+}
+
+export function createMocktoFn(context: AsyncContext<Spec.Context>, mode?: Spec.Mode) {
+  const specFn = (...args: any[]) => {
+    const { specName, options = { timeout: 3000 }, handler } = resolveMocktoFnArgs(args)
+    const reporter = createMemoryLogReporter()
+    const specRelativePath = getCallerRelativePath(specFn)
+
+    handler(specName,
+      createSpecObject(
+        context
+          .extend({ reporter, specName, specRelativePath })
+          .extend(getEffectiveSpecModeContext(mode))
+          .extend(createLogContext),
+        specName,
+        options),
+      reporter)
+  }
+  return specFn as createMockto.MocktoFn
 }
