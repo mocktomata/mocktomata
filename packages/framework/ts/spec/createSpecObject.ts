2ecb588aeacfa9ce0e64e179e8ef87f49f9796fc
Homa Wong <homawong@gmail.com>
2022-10-16 00:05:31 -0700

    chore: add createSpecFns
    which breaks up the spec functions so it can be composed differently for scenario

--- packages/framework/ts/spec/createSpecObject.ts
+++ packages/framework/ts/spec/createSpecObject.ts
@@ -1,10 +1,18 @@
 import type { AsyncContext } from 'async-fp'
 import { LogLevel, logLevels } from 'standard-log'
 import { createSpec } from './createSpec.js'
-import type { Spec } from './types.js'
 import { InvokeMetaMethodAfterSpec } from './errors.js'
+import type { Spec } from './types.js'
 
 export function createSpecObject(context: AsyncContext<Spec.Context & {
+  mode: Spec.Mode,
+  specRelativePath: string,
+}>, specName: string, options: Spec.Options) {
+  const { spec, mode, enableLog, ignoreMismatch, maskValue, done } = createSpecFns(context, specName, options)
+  return Object.assign(Object.defineProperties(spec, { mode }), { enableLog, ignoreMismatch, maskValue, done }) as any
+}
+
+export function createSpecFns(context: AsyncContext<Spec.Context & {
   mode: Spec.Mode,
   specRelativePath: string,
 }>, specName: string, options: Spec.Options) {
@@ -20,37 +28,44 @@ export function createSpecObject(context: AsyncContext<Spec.Context & {
     initState.maskCriteria.forEach(v => spec.maskValue(v.value, v.replaceWith))
     return spec
   }
+  const { resolve, promise } = defer<Spec>()
 
-  const spec = Object.assign((subject: any) => createActualSpec(initState).then(actualSpec => {
-    if (!spec.mode) {
-      Object.defineProperties(spec, {
-        mode: {
-          get() { return actualSpec.mode }
-        },
-        done: {
-          configurable: false,
-          value: actualSpec.done,
-          writable: false
-        }
-      })
+  function enableLog(level: LogLevel = logLevels.all) {
+    if (s) s.enableLog(level)
+    else {
+      initState.enableLog = true
+      initState.logLevel = level
     }
+  }
+  function ignoreMismatch(value: any) {
+    if (s) throw new InvokeMetaMethodAfterSpec('ignoreMismatch')
+    else initState.ignoreValues.push(value)
+  }
+  function maskValue(value: any, replaceWith: any) {
+    if (s) throw new InvokeMetaMethodAfterSpec('maskValue')
+    initState.maskCriteria.push({ value, replaceWith })
+  }
+
+  let actualMode: Spec.Mode
+  const mode = {
+    get() { return actualMode }
+  }
+  function done() { return promise.then(s => s.done()) }
+  const spec = (subject: any) => createActualSpec(initState).then(actualSpec => {
+    actualMode = actualSpec.mode
+    resolve(actualSpec)
     return actualSpec(subject)
-  }), {
-    enableLog: (level: LogLevel = logLevels.all) => {
-      if (s) s.enableLog(level)
-      else {
-        initState.enableLog = true
-        initState.logLevel = level
-      }
-    },
-    ignoreMismatch(value: any) {
-      if (s) throw new InvokeMetaMethodAfterSpec('ignoreMismatch')
-      else initState.ignoreValues.push(value)
-    },
-    maskValue(value: any, replaceWith: any) {
-      if (s) throw new InvokeMetaMethodAfterSpec('maskValue')
-      initState.maskCriteria.push({ value, replaceWith })
-    }
-  }) as any
-  return spec
+  })
+  return { spec, mode, enableLog, ignoreMismatch, maskValue, done }
+}
+
+function defer<T>() {
+  let resolve: (value: T | PromiseLike<T>) => void
+  let reject: (reason?: any) => void
+  const promise = new Promise<T>((a, r) => {
+    resolve = a
+    reject = r
+  })
+
+  return { resolve: resolve!, reject: reject!, promise }
 }
