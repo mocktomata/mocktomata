2b20281c51c687c78419863ac3baf9122982e3dc
Homa Wong <homawong@gmail.com>
2022-10-18 00:00:33 -0700

    test: add tests

--- .jest/jest.nodejs.js
+++ .jest/jest.nodejs.js
@@ -1,7 +1,9 @@
 const base = require('./jest.base')
+const watch = require('./jest.watch')
 
 module.exports = {
   ...base,
   testEnvironment: 'node',
   testMatch: ['**/?*.(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
+  ...watch
 }
