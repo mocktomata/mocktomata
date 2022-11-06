597928118e54c400e8bfe42da66e0ce66173aac3
Homa Wong <homawong@gmail.com>
2022-11-06 10:42:56 -0800

    chore: update satisfier to use hasAll

--- packages/nodejs/ts/plugin/findInstalledPlugins.spec.ts
+++ packages/nodejs/ts/plugin/findInstalledPlugins.spec.ts
@@ -2,7 +2,7 @@ import { a } from 'assertron'
 import { dirSync } from 'tmp'
 import { findInstalledPlugins } from './index.js'
 import { fixturePath } from '../test_util/fixturePath.js'
-import { has } from 'satisfier'
+import { hasAll } from 'satisfier'
 test('gets empty plugin list in empty folder', async () => {
   expect(await findInstalledPlugins(dirSync().name)).toEqual([])
 })
@@ -10,7 +10,7 @@ test('gets empty plugin list in empty folder', async () => {
 test('find all installed plugins', async () => {
   const cwd = fixturePath('has-plugins')
 
-  a.satisfies(await findInstalledPlugins(cwd), has(
+  a.satisfies(await findInstalledPlugins(cwd), hasAll(
     '@mocktomata/plugin-fixture-deep-link',
     '@mocktomata/plugin-fixture-dummy'
   ))
