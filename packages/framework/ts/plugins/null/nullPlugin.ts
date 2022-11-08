2159a3cfc52d4d717608f428ac89f231a47e83b9
Homa Wong <homawong@gmail.com>
2022-11-08 00:18:52 -0800

    fix: adjust null log

--- packages/framework/ts/plugins/null/nullPlugin.ts
+++ packages/framework/ts/plugins/null/nullPlugin.ts
@@ -3,6 +3,6 @@ import type { SpecPlugin } from '../../spec-plugin/types.js'
 export const nullPlugin: SpecPlugin<null> = {
   name: 'null',
   support: subject => subject === null,
-  createSpy: () => null,
+  createSpy: ({ setMeta }) => (setMeta(null), null),
   createStub: () => null
 }
