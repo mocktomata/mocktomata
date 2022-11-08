00b5818f3c7aeafd286f3cd9f8c4299f0d25947c
Homa Wong <homawong@gmail.com>
2022-11-08 01:14:44 -0800

    test: update more tests

--- packages/framework/ts/es2015.spec.ts
+++ packages/framework/ts/es2015.spec.ts
@@ -1729,11 +1729,12 @@ describe('maskValue', () => {
     })
   })
 
-  incubator.save('call after spec throws', (specName, spec) => {
+  incubator.save('call after spec throws', { logLevel: logLevels.all }, (specName, spec, reporter) => {
     test(specName, async () => {
       await spec(() => { })
       a.throws(() => spec.maskValue('secret'), InvokeMetaMethodAfterSpec)
     })
+    expect(reporter.getLogMessage()).not.toContain('secret')
   })
 
   incubator('not save masked value in log', { logLevel: logLevels.all }, (specName, spec, reporter) => {
@@ -1742,53 +1743,58 @@ describe('maskValue', () => {
       const s = await spec((v: string) => v)
       s('secret')
       await spec.done()
-      expect(JSON.stringify(reporter.logs)).not.toMatch('secret')
+      expect(reporter.getLogMessage()).not.toContain('secret')
     })
   })
 
-  incubator('invoke returns masked value', (specName, spec) => {
+  incubator('invoke returns masked value', { logLevel: logLevels.all }, (specName, spec, reporter) => {
     test(specName, async () => {
       spec.maskValue('secret')
       const s = await spec((v: string) => v)
       const actual = s('secret')
       expect(actual).toBe('[masked]')
       await spec.done()
+      expect(reporter.getLogMessage()).not.toContain('secret')
     })
   })
 
-  incubator('get returns masked value', (specName, spec) => {
+  incubator('get returns masked value', { logLevel: logLevels.all }, (specName, spec, reporter) => {
     test(specName, async () => {
       spec.maskValue('secret')
-      const s = await spec({ secret: 'secret' })
-      expect(s.secret).toBe('[masked]')
+      const s = await spec({ someProp: 'secret' })
+      expect(s.someProp).toBe('[masked]')
       await spec.done()
+      expect(reporter.getLogMessage()).not.toContain('secret')
     })
   })
 
-  incubator('against array', (specName, spec) => {
+  incubator('against array', { logLevel: logLevels.all }, (specName, spec, reporter) => {
     test(specName, async () => {
       spec.maskValue('secret')
       const s = await spec((v: string) => [v, 'world'])
       expect(s('secret')).toEqual(['[masked]', 'world'])
       await spec.done()
+      expect(reporter.getLogMessage()).not.toContain('secret')
     })
   })
 
-  incubator('against object', (specName, spec) => {
+  incubator('against object', { logLevel: logLevels.all }, (specName, spec, reporter) => {
     test(specName, async () => {
       spec.maskValue('secret')
       const s = await spec((value: string) => { return { value, b: 1 } })
       expect(s('secret')).toEqual({ value: '[masked]', b: 1 })
       await spec.done()
+      expect(reporter.getLogMessage()).not.toContain('secret')
     })
   })
 
-  incubator('against object containing null', (specName, spec) => {
+  incubator('against object containing null', { logLevel: logLevels.all }, (specName, spec, reporter) => {
     test(specName, async () => {
       spec.maskValue('secret')
       const s = await spec((value: string) => { return { value, b: null } })
       expect(s('secret')).toEqual({ value: '[masked]', b: null })
       await spec.done()
+      expect(reporter.getLogMessage()).not.toContain('secret')
     })
   })
 })
