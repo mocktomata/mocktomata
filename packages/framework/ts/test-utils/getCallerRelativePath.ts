9561c8a4dec807742d0f398c2fbdc2eeb96c8aac
unional <homawong@gmail.com>
2022-11-28 03:48:43 -0800

    fix: errorPlugin to handle inheritance

--- packages/framework/ts/test-utils/getCallerRelativePath.ts
+++ packages/framework/ts/test-utils/getCallerRelativePath.ts
@@ -3,6 +3,10 @@ import type { AnyFunction } from 'type-plus'
 
 const stackUtil = new StackUtils({ cwd: process.cwd() })
 
+export function getCallSites() {
+  return stackUtil.capture().slice(1)
+}
+
 export function getCallerRelativePath(subject: AnyFunction): string {
   const callsite = stackUtil.at(subject)
   const raw = callsite.file || callsite.evalOrigin
