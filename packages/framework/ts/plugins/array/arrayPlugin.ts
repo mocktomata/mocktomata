c7974ff28499b7a3e6f950054271715a4c9c7ed1
unional <homawong@gmail.com>
2022-12-02 17:50:28 -0800

    fix: array containing object

--- packages/framework/ts/plugins/array/arrayPlugin.ts
+++ packages/framework/ts/plugins/array/arrayPlugin.ts
@@ -3,9 +3,10 @@ import type { SpecPlugin } from '../../spec-plugin/types.js'
 export const arrayPlugin: SpecPlugin<any[], any[]> = {
   name: 'array',
   support: Array.isArray,
-  createSpy({ setMeta, getSpyId, setProperty }, subject) {
-    setMeta(subject.map(item => getSpyId(item)))
-    return new Proxy(subject, {
+  createSpy({ setMeta, getSpy, getSpyId, setProperty }, subject) {
+    const spiedSubject = subject.map(getSpy)
+    setMeta(spiedSubject.map(getSpyId))
+    return new Proxy(spiedSubject, {
       set(target: any, property: any, value: any) {
         return setProperty({ key: property, value }, value => target[property] = value)
       }
