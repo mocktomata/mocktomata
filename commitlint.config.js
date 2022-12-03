8dee82f49e66a711fa7ffb404fad93a708e58858
unional <homawong@gmail.com>
2022-12-02 16:51:31 -0800

    chore: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    "body-max-line-length": [0]
+    'subject-case': [0],
+    'header-case': [0],
+    'header-max-length': [0],
+    'body-max-line-length': [0]
   }
 }
