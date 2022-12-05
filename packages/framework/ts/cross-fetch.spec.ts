ea2d40e621a607cabb2bd9e48db6eb0ef6445dfc
unional <homawong@gmail.com>
2022-12-04 20:19:37 -0800

    fix: array.sort()

---
+++ packages/framework/ts/cross-fetch.spec.ts
@@ -0,0 +1,13 @@
+import { fetch } from 'cross-fetch'
+import { incubator } from './index.js'
+
+incubator('find kidnap worthy breed', (specName, spec) => {
+  it(specName, async () => {
+    const f = await spec(fetch)
+    const r = await f('https://api.thecatapi.com/v1/breeds?limit=10')
+    const breeds: any[] = await r.json()
+    const dumbest = breeds.sort((a, b) => a.intelligence - b.intelligence)[0]
+    expect(dumbest.intelligence).toEqual(3)
+    await spec.done()
+  })
+})
