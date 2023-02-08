8a990e724799af9e3077985a079e838cd7433cdf
unional <homawong@gmail.com>
2023-02-07 21:20:09 -0800

    chore: update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,11 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  editorconfig: true,
+  arrowParens: 'avoid',
   printWidth: 110,
-  endOfLine: 'auto',
   semi: false,
   singleQuote: true,
-  useTabs: true,
   // For ES5, trailing commas cannot be used in function parameters; it is counterintuitive
   // to use them for arrays only
-  trailingComma: 'none'
+  trailingComma: 'none',
+  useTabs: true,
 };
