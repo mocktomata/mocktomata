c5102da6e251fd025891f1b99346ded4e609f6b4
unional <homawong@gmail.com>
2023-02-11 13:09:38 -0800

    chore: remove top-level jest
    Use turbo for all tests

--- packages/mocktomata/jest.electron.cjs
+++
@@ -1,6 +0,0 @@
-const base = require('../../.jest/jest.electron')
-
-module.exports = {
-	...base,
-	displayName: 'mocktomata:electron'
-}
---
+++ packages/mocktomata/jest.jsdom.mjs
@@ -0,0 +1,9 @@
+import preset from '@repobuddy/jest/presets/jsdom-ts-watch'
+import deepmerge from 'deepmerge'
+import localPreset from '../../.jest/preset.js'
+
+/** @type {import('jest').Config} */
+export default deepmerge(preset, {
+	moduleNameMapper: localPreset.moduleNameMapper,
+	displayName: 'mocktomata:jsdom'
+})
