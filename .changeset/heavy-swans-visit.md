728b79126558229c20961e133a965b51ad75e30b
Homa Wong <homawong@gmail.com>
2023-02-17 11:41:04 -0800

    fix: add browser spec field to exports

---
+++ .changeset/heavy-swans-visit.md
@@ -0,0 +1,8 @@
+---
+'mocktomata': patch
+---
+
+Add browser field spec in the `exports` field.
+And it has to come before `import`.
+
+Also create a stub version of the mocktomata.
--- packages/mocktomata/ts/browser/index.ts
+++ packages/mocktomata/ts/browser/index.ts
@@ -1,2 +1,15 @@
-export * from './config.js'
-export * from './mockto.js'
+import { createKomondor, createMockto, createTestContext, createZucchini } from '@mocktomata/framework'
+
+export { incubator } from '@mocktomata/framework'
+
+const { context, config } = createTestContext()
+const { scenario, defineStep, defineParameterType } = createZucchini(context)
+
+export { config }
+export { scenario, defineStep, defineParameterType }
+
+export const komondor = createKomondor(context)
+export const kd = komondor
+
+export const mockto = createMockto(context)
+export const mt = mockto
