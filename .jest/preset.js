8db8d6854b2fce48a86dcccc136c402258dc838f
unional <homawong@gmail.com>
2023-02-19 14:11:41 -0800

    chore: adjust tsconfig setting

--- .jest/preset.js
+++ .jest/preset.js
@@ -1,6 +1,7 @@
 module.exports = {
 	moduleNameMapper: {
 		'^@mocktomata/(plugin-fixture-deep-link.*)': '<rootDir>/../../test-plugins/$1',
+		'^@mocktomata/(.*)/(.*)': '<rootDir>/../../packages/$1/ts/$2',
 		'^@mocktomata/(.*)': '<rootDir>/../../packages/$1/ts',
 		'^mocktomata': '<rootDir>/../../packages/mocktomata/ts'
 	},
