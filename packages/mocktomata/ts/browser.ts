5058d9af810eadfd4a634c60fa053f6ace0a8b59
unional <homawong@gmail.com>
2023-02-19 14:46:21 -0800

    refactor: move exports into ./framework

--- packages/mocktomata/ts/browser.ts
+++ packages/mocktomata/ts/browser.ts
@@ -1,11 +1,2 @@
-export type {
-	Komondor,
-	MismatchActionModel,
-	Mocktomata,
-	Spec,
-	SpecPlugin,
-	SpecRecord,
-	Zucchini,
-} from '@mocktomata/framework'
 export * from './framework.js'
 export * from './browser/index.js'
--- packages/mocktomata/ts/index.ts
+++ packages/mocktomata/ts/index.ts
@@ -1,12 +1,4 @@
-export type {
-	Komondor,
-	MismatchActionModel,
-	Mocktomata,
-	Spec,
-	SpecPlugin,
-	SpecRecord,
-	Zucchini
-} from '@mocktomata/framework'
 export { start as startServer } from '@mocktomata/service'
 export * from './framework.js'
 export * from './nodejs/index.js'
+
