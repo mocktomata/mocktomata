60102758e8b55cdaf65132ddcfc3ec7646c26610
unional <homawong@gmail.com>
2023-02-20 01:15:18 -0800

    feat: change url to /api

--- packages/service/ts/start.spec.ts
+++ packages/service/ts/start.spec.ts
@@ -6,16 +6,16 @@ import { a } from 'assertron'
 import fs, { readFileSync } from 'fs'
 import f from 'node-fetch'
 import path from 'path'
-import { createStandardLog } from 'standard-log'
+import { createStandardLogForTest } from 'standard-log'
 import { dirSync } from 'tmp'
 import { btoa } from './base64.js'
 import { start } from './index.js'
 
 const fetch = f.default
 
-test('if a port is specified and not available, will throw an error', async () => {
+it('throws EADDRINUSE if the specified port is being used (hapi)', async () => {
 	const runningServer = await start({ port: 3710 })
-	const e = await a.throws<Error & Record<string, any>>(start({ port: Number(runningServer.info.port) }))
+	const e = await a.throws<Error & { code: string }>(start({ port: Number(runningServer.info.port) }))
 	await runningServer.stop()
 	expect(e.code).toBe('EADDRINUSE')
 })
@@ -26,20 +26,18 @@ describe('server behavior', () => {
 	beforeAll(async () => {
 		const cwd = dirSync().name
 		fs.writeFileSync(path.join(cwd, 'package.json'), json.stringify({ mocktomata: { logLevel: 'trace' } }))
-		const sl = createStandardLog()
+		const sl = createStandardLogForTest()
 		const log = sl.getLogger('test')
 		const io = createIO({ cwd, log })
 		await io.writeSpec('exist', '', { actions: [], refs: [] })
-		log.info('file-server test: starting server...')
 		server = await start({ cwd, port: 3456 })
-		log.info('file-server test: started server...', json.stringify(server.info, undefined, '\n'))
 	})
 	afterAll(() => {
 		return server.stop()
 	})
 
 	function buildUrl(path: string) {
-		return `http://${server.info.address}:${server.info.port}/mocktomata/${path}`
+		return `http://${server.info.address}:${server.info.port}/api/${path}`
 	}
 
 	test('get mocktomata info', async () => {
