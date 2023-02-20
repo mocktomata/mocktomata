867df3be0e112826695a226dbd177f0cd6b0b0e5
unional <homawong@gmail.com>
2023-02-20 12:59:48 -0800

    chore: add start/stop server

--- packages/io-remote/jest.electron.mjs
+++ packages/io-remote/jest.electron.mjs
@@ -1,6 +1,9 @@
 import base from '../../.jest/jest.electron.render.js'
 
+/** @type {import('jest').Config} */
 export default {
 	...base,
-	displayName: 'io-remote'
+	displayName: 'io-remote',
+	globalSetup: './scripts/start_server.cjs',
+	globalTeardown: './scripts/stop_server.cjs'
 }
