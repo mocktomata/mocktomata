c7458885410f7971cbb3a33f0d689bcd83913569
unional <homawong@gmail.com>
2023-02-20 01:33:48 -0800

    fix(cli): ESM usage due to require()

--- packages/cli/ts/cli.ts
+++ packages/cli/ts/cli.ts
@@ -1,7 +1,8 @@
 import { cli } from 'clibuilder'
 import { serveCommand } from './serve_command.js'
+import require from 'uni-require'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const pjson = require('../../package.json')
+const pjson = require('./package.json')
 
 export const app = cli({ name: 'mtmt', version: pjson.version }).command(serveCommand)
