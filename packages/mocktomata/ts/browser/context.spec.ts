08950badb7653749fa51a7d334df6199d3a70c04
unional <homawong@gmail.com>
2023-02-21 01:58:23 -0800

    fix: add and update newContext
    still in progress

--- packages/mocktomata/ts/browser/context.spec.ts
+++ packages/mocktomata/ts/browser/context.spec.ts
@@ -1,10 +1,20 @@
+import { CannotConfigAfterUsed } from '@mocktomata/framework'
+import { a } from 'assertron'
 import { newContext } from './context.js'
 
+describe(`getConfig()`, () => {
+	it(`throws ${CannotConfigAfterUsed.name} after context is constructed.`, async () => {
+		const { config, getContext } = newContext()
+		await getContext().asyncContext.get()
+		a.throws(config, CannotConfigAfterUsed)
+	})
+})
+
 describe(`getContext()`, () => {
-	it(`defaults url to http://localhost:3698`, () => {
+	it(`defaults url to http://localhost:3698`, async () => {
 		const { getContext } = newContext()
-		const ctx = getContext()
-		const x = ctx.stackFrame.getCallSites()
-		console.info(x)
+		const r = await getContext().asyncContext.get()
+		// able to read meaning it is reading from the service on port 3698
+		expect(r.config.ecmaVersion).toEqual(`es2020`)
 	})
 })
