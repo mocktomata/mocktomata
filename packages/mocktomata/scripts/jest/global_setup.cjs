c74a9f94041958987ca6ce1232581ce301802a37
unional <homawong@gmail.com>
2023-02-21 02:16:33 -0800

    fix: support multiple servers

--- packages/mocktomata/scripts/jest/global_setup.cjs
+++ packages/mocktomata/scripts/jest/global_setup.cjs
@@ -4,4 +4,8 @@ module.exports = async function () {
 	await globalSetup({
 		cwd: './fixtures/service'
 	})
+	await globalSetup({
+		cwd: './fixtures/es2015',
+		port: 3699
+	})
 }
--- packages/service/ts/jest.ts
+++ packages/service/ts/jest.ts
@@ -3,9 +3,11 @@ import { start } from './start.js'
 
 export async function globalSetup(options?: start.Options) {
 	const server = await start(required({ port: 3698, cwd: process.cwd() }, options))
-	;(globalThis as any).__SERVER__ = server
+	const servers = ((globalThis as any).__SERVERS__ = (globalThis as any).__SERVERS__ ?? [])
+	servers.push(server)
 }
 
 export async function globalTeardown() {
-	await (globalThis as any).__SERVER__.stop()
+	const servers = (globalThis as any).__SERVERS__
+	if (servers) await Promise.all(servers.map((s: any) => s.stop()))
 }
