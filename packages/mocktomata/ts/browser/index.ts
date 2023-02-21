c210dd05a697ef4454b597a458fb91f489faaee6
unional <homawong@gmail.com>
2023-02-20 16:16:20 -0800

    chore: clean up exports

--- packages/mocktomata/ts/browser/index.ts
+++ packages/mocktomata/ts/browser/index.ts
@@ -1,12 +1,11 @@
 import { createKomondor, createMockto, createZucchini } from '@mocktomata/framework'
 import { createContext } from './context.js'
 
-export { incubator } from '@mocktomata/framework'
-export { config }
-export { scenario, defineStep, defineParameterType }
-
 const { context, config, stackFrame } = createContext()
 const { scenario, defineStep, defineParameterType } = createZucchini({ context, stackFrame })
+
+export { config }
+export { scenario, defineStep, defineParameterType }
 
 export const komondor = createKomondor({ context, stackFrame })
 export const kd = komondor
