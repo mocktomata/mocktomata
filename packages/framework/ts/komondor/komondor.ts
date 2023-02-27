102359f21667ad0b10557dfddd2de5fd22f0a421
unional <homawong@gmail.com>
2023-02-26 16:26:33 -0800

    refactor: rename context.ts to context.internal.ts
    Need to rework the context design

--- packages/framework/ts/komondor/komondor.ts
+++ packages/framework/ts/komondor/komondor.ts
@@ -1,7 +1,7 @@
 import type { AsyncContext } from 'async-fp'
 import { createMemoryLogReporter, MemoryLogReporter } from 'standard-log'
 import { loadConfig } from '../config/index.js'
-import type { LoadedContext } from '../context.js'
+import type { LoadedContext } from '../context.internal.js'
 import { createLogContext } from '../log/log_context.js'
 import { createSpecObject, getEffectiveSpecModeContext, type Spec } from '../spec/index.js'
 import type { MockSpec } from '../spec/types.js'
